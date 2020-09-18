c0002ec0ce59362596501b43cdadb6b7fffbda4d
unknown <mbhe.03@gmail.com>
2020-09-18 17:55:04 -0500

    agregar estructura proyecto

---
+++ lib/pos/presentation/cart/cubit/card_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:facilapp/pos/data/repositories/book_repository.dart';
+import 'package:facilapp/pos/models/book.dart';
+
+part 'card_state.dart';
+
+class CardCubit extends Cubit<CardState> {
+  CardCubit() : super(CardInitial());
+
+  final _bookRepository = BookRepository();
+
+  void registra(Book book) async {
+    int code = await _bookRepository.registerBook(name: book.name);
+    if (code == 200) {
+      emit(RegisteredBook());
+    }
+  }
+}
